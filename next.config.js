a6e2dcee3add74cf75171c922ee1bbae5ca7667e
digital evolutions <digitalevolutions@digitals-MacBook-Air.local>
2023-11-03 00:58:05 +0530

    Perpared for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+const nextConfig = (phase) => {
+
+  if(phase === PHASE_DEVELOPMENT_SERVER){
+    return {
+      env: {
+        mongodb_username: 'nextdathil',
+        mongodb_password: '5hGPUwnGCqIpHUgz',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'nextdevnetsdev',
+      },
+      reactStrictMode: true,
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: 'nextdathil',
+      mongodb_password: '5hGPUwnGCqIpHUgz',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'nextdevnets',
+    },
+    reactStrictMode: true,
+  }
+  
 }
 
 module.exports = nextConfig
